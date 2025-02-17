d35ed6fa8d8f54819d9aeb5ea4f97f5878ced4c4
solvvitalik <solvvitalik@gmail.com>
2025-02-17 18:04:31 +0100

    yh

--- app/trade/components/trade-swap.tsx
+++ app/trade/components/trade-swap.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useAccount, useWriteContract } from "wagmi";
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 import { ArrowRightLeft, Gem } from "lucide-react";
@@ -5,51 +8,32 @@ import Image from "next/image";
 import React from "react";
 
 const TradeSwap = () => {
+    const { address, isConnected } = useAccount();
+  
   return (
     <div className="flex justify-center items-center w-full min-h-96">
+    {isConnected || address ? (
+
       <div className="flex flex-col md:flex-row items-center gap-12 my-5">
         <Card className="overflow-hidden rounded-2xl w-56">
           <div className="relative aspect-square">
             <Image
-              src={"/collections/beanz002.png"}
+              src={"/collections/989.png"}
               alt={`ON1 jn`}
               fill
               className="object-cover"
             />
           </div>
           <div className="p-4">
-            <div className="text-sm text-muted-foreground">Bean #1683</div>
+            <div className="text-sm text-muted-foreground">Quirkies #3935</div>
             <div className="flex items-center justify-between">
-              <div className="">BEANZ Official</div>
+              <div className="">Quirkies Originals</div>
               <Badge
                 variant="secondary"
                 className="flex items-center gap-2 text-[#614BC3]"
               >
                 <Gem className="h-3 w-3 " />
-                <div className="">1683</div>
-              </Badge>
-            </div>
-          </div>
-        </Card>
-        <Card className="overflow-hidden rounded-2xl w-56">
-          <div className="relative aspect-square">
-            <Image
-              src={"/collections/beanz001.png"}
-              alt={`ON1 jn`}
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div className="p-4">
-            <div className="text-sm text-muted-foreground">Bean #200</div>
-            <div className="flex items-center justify-between">
-              <div className="">BEANZ Official</div>
-              <Badge
-                variant="secondary"
-                className="flex items-center gap-2 text-[#614BC3]"
-              >
-                <Gem className="h-3 w-3 " />
-                <div className="">200</div>
+                <div className="">1553</div>
               </Badge>
             </div>
           </div>
@@ -67,7 +51,7 @@ const TradeSwap = () => {
             />
           </div>
           <div className="p-4">
-            <div className="text-sm text-muted-foreground">1.2eth</div>
+            <div className="text-sm text-muted-foreground">6.0Eth</div>
             <div className="flex items-center justify-between">
               <div className="">Ethereum Chain</div>
               <Badge
@@ -81,7 +65,10 @@ const TradeSwap = () => {
           </div>
         </Card>
       </div>
+    ) : null}
+
     </div>
+    
   );
 };
 
