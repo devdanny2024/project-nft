9c9c3022872fae39944cd731543709980d231170
solvvitalik <solvvitalik@gmail.com>
2025-02-20 19:23:32 +0100

    Yh

--- app/trade/components/trade-swap.tsx
+++ app/trade/components/trade-swap.tsx
@@ -18,22 +18,45 @@ const TradeSwap = () => {
         <Card className="overflow-hidden rounded-2xl w-56">
           <div className="relative aspect-square">
             <Image
-              src={"/collections/bluebean.png"}
+              src={"/collections/b1.png"}
               alt={`ON1 jn`}
               fill
               className="object-cover"
             />
           </div>
           <div className="p-4">
-            <div className="text-sm text-muted-foreground">Bean #16224</div>
+            <div className="text-sm text-muted-foreground">The Bullas #5966</div>
             <div className="flex items-center justify-between">
-              <div className="">BEANZ Official</div>
+              <div className="">bullas</div>
               <Badge
                 variant="secondary"
                 className="flex items-center gap-2 text-[#614BC3]"
               >
                 <Gem className="h-3 w-3 " />
-                <div className="">16224</div>
+                <div className="">5966</div>
+              </Badge>
+            </div>
+          </div>
+        </Card>
+        <Card className="overflow-hidden rounded-2xl w-56">
+          <div className="relative aspect-square">
+            <Image
+              src={"/collections/b2.png"}
+              alt={`ON1 jn`}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="p-4">
+            <div className="text-sm text-muted-foreground">The Bullas #5128</div>
+            <div className="flex items-center justify-between">
+              <div className="">bullas</div>
+              <Badge
+                variant="secondary"
+                className="flex items-center gap-2 text-[#614BC3]"
+              >
+                <Gem className="h-3 w-3 " />
+                <div className="">5966</div>
               </Badge>
             </div>
           </div>
@@ -44,16 +67,39 @@ const TradeSwap = () => {
         <Card className="overflow-hidden rounded-2xl w-56">
           <div className="relative aspect-square">
             <Image
-              src={"/collections/mygirl.png"}
+              src={"/collections/dooplicator.gif"}
               alt={`ON1 jn`}
               fill
               className="object-cover"
             />
           </div>
           <div className="p-4">
-            <div className="text-sm text-muted-foreground">Elemental #19920</div>
+            <div className="text-sm text-muted-foreground">Dooplicator #6287</div>
             <div className="flex items-center justify-between">
-              <div className="">Azuki Elementals</div>
+              <div className="">Dooplicator</div>
+              <Badge
+                variant="secondary"
+                className="flex items-center gap-2 text-[#614BC3]"
+              >
+                <Gem className="h-3 w-3 " />
+                <div className=""></div>
+              </Badge>
+            </div>
+          </div>
+        </Card>
+        <Card className="overflow-hidden rounded-2xl w-56">
+          <div className="relative aspect-square">
+            <Image
+              src={"/collections/eth.png"}
+              alt={`ON1 jn`}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="p-4">
+            <div className="text-sm text-muted-foreground">0.3eth</div>
+            <div className="flex items-center justify-between">
+              <div className="">Mainet Network</div>
               <Badge
                 variant="secondary"
                 className="flex items-center gap-2 text-[#614BC3]"
